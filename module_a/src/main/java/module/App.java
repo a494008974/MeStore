6086e8e8a4ce965c4efadd6c865f9185680260da
zhou <494008974@qq.com>
2018-07-10 18:34:33 +0800

    ee

---
+++ module_a/src/main/java/module/App.java
@@ -0,0 +1,19 @@
+package module;
+
+import android.app.Application;
+import android.support.multidex.MultiDex;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+
+/**
+ * Created by Administrator on 2018/7/10.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MultiDex.install(this);
+        ARouter.init(this);
+    }
+}
